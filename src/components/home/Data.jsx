cdc1edd8e4453467ca6c4102276909b9fc24607a
AnasKhalif <anaskhalif995@gmail.com>
2024-05-25 13:27:32 +0700

    feat add page home

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -56,7 +56,7 @@ const Data = () => {
         </svg>
       </h1>
 
-      <h3 className="home__subtitle">Full-Stack Developer🎯</h3>
+      <h3 className="home__subtitle">Full-Stack Developer 🎯</h3>
       <p className="home__description">
         Hi, I'm Anas Khalif. A passionate Full-Stack Developer based in Kediri,
         Indonesia. 📍
